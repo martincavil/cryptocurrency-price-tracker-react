931aad5fdd3830f37ec68b83a34a56d8d43c03ad
Martin Cavil <martincavil.dev@gmail.com>
2022-06-02 12:39:50 +0200

    called currencies

--- src/components/coin/Coin.jsx
+++ src/components/coin/Coin.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import './coin.scss';
 
-export default function Coin() {
+export default function Coin({ name, image, symbol, price, volume}) {
   return (
     <div className='coinContainer'>
       <div className="coinRow">
         <div className="coin">
           <img src={image} alt="crypto" />
-          <h1></h1>
+          <h1>Name</h1>
           <p className='coinSymbol'>{symbol}</p>
         </div>
         <div className="coinData">
           <p className='coinPrice'>{price}</p>
-          <p className='coinVolum'>{volume}</p>
+          <p className='coinVolum'>{volume.toString()}</p>
+
         </div>
       </div>
     </div>
